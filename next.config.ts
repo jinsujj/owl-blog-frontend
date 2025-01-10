fb5c539a93089316bba26509c36e97dfe6fdc31c
jinsujj <wlstncjs1234@naver.com>
2025-01-10 14:26:14 +0900

    styled-component ssr 지원 css 클래스명 안정성 설정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/*
+		SSR support: Ensures styles match between server and client.
+		CSS class stability: Generates consistent class names for server and client.
+		Improved performance: Faster compilation with SWC instead of Babel.
+	*/
+	compiler: {
+    styledComponents: true,  
+  },
 };
 
 export default nextConfig;
