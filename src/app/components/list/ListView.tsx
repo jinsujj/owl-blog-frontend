4bb1b24dfb19e54a88fcd3cac1675e0641a62742
jinsujj <wlstncjs1234@naver.com>
2025-01-12 16:08:13 +0900

    ListView 컴포넌트화

---
+++ src/app/components/list/ListView.tsx
@@ -0,0 +1,103 @@
+import { Post } from "@/app/api/blogApi";
+import { useSelector } from "@/app/store";
+import styled, { keyframes } from "styled-components";
+
+
+interface StyledProps {
+	$isDark: boolean;
+}
+
+const fadeIn = keyframes`
+    from {
+        opacity: 0;
+        transform: translateY(10px);
+    }
+
+    to {
+        opacity: 1;
+        transform: translateY(0);
+    }
+`;
+
+const ListViewContainer = styled.div<StyledProps>`
+    padding-top: 16px;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    animation: ${fadeIn} 0.3s ease-in-out;
+
+    .list-item {
+        display: flex;
+        align-items: center;
+        gap:20px;
+        padding: 15px;
+        border-radius: 10px;
+        background-color: ${(props) => (props.$isDark? "#444" : "#fff")};
+        box-shadow: 0 4px 6px rgba(0,0,0, 0.1);
+        transition: transform 0.2s ease, box-shadow 0.2s ease;
+
+        &:hover {
+            transform: translateY(-5px);
+            box-shadow: 0 6px 12px rgba(0,0,0, 0.15);
+        }
+    }
+
+    .thumbnail {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 120px;
+        height: 120px;
+        flex-shrink: 0;
+        border-radius: 8px;
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        box-shadow: 0 2px 4px rgba(0,0,0 0.1);
+        margin-bottom: -2px;
+    }
+
+    .content {
+        flex-grow: 18px;
+        font-weight: bold;
+        margin-bottom: 5px;
+        color: ${(props: StyledProps) => (props.$isDark ? "#fff": "#333")};
+    }
+
+    .summary {
+        font-size: 14px;
+        line-height: 1.4;
+        color: ${(props: StyledProps) => (props.$isDark ? "#ddd" : "#666") };
+    }
+
+    .meta {
+        font-size: 12px ;
+        color: ${(props: StyledProps) => (props.$isDark ? "#aaa" : "#999")};
+        margin-top: 10px;
+    }
+`;
+
+interface ListViewProps {
+  posts: Post[]; 
+}
+
+const ListView = ({posts}: ListViewProps) => {
+    const isDarkMode = useSelector((state) => state.common.isDark);
+    
+    return (
+        <ListViewContainer $isDark={isDarkMode}>
+            {posts.map((post) => (
+                <div className="list-item" key={post.id}>
+                    <div className="thumbnail" style={{ backgroundImage: `url(${post.thumbnail})` }}/>
+                    <div className="content">
+                        <div className="title">{post.title}</div>
+                        <div className="summary">	{post.summary.length > 250 ? `${post.summary.slice(0,250)}...` : post.summary}</div>
+                        <div className="meta">Updated at: {post.updatedAt}</div>
+                    </div>
+                </div>
+            ))}
+        </ListViewContainer>
+    )
+}
+
+export default ListView;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,26 +2,16 @@
 
 import Header from "./components/common/Header";
 import { useSelector } from "./store";
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 import CardList from "./components/card/CardList";
 import { UserProfile } from "./components/common/UserProfile";
 import { useState } from "react";
 import { HiMiniSquares2X2, HiBars3 } from "react-icons/hi2";
+import ListView from "./components/list/ListView";
 
 interface StyledProps {
 	$isDark: boolean;
 }
-
-const fadeIn = keyframes`
-  from {
-    opacity: 0;
-    transform: translateY(10px);
-  }
-  to {
-    opacity: 1;
-    transform: translateY(0);
-  }
-`;
 
 const PageContainer = styled.div<StyledProps>`
   padding: 0;
@@ -39,7 +29,7 @@ const MainWrapper = styled.div`
 `;
 
 const Main = styled.main`
-	max-width: 1200px;
+	max-width: 980px;
 	width: 100%;
   padding: 20px;
 	display: flex;
@@ -67,71 +57,6 @@ const ToggleButton = styled.button`
 
   &:hover {
     background-color: #ddd;
-  }
-`;
-
-const ListView = styled.div<StyledProps>`
-	padding-top: 16px;
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
-	animation: ${fadeIn} 0.3s ease-in-out;
-
-  .list-item {
-    display: flex;
-    align-items: center;
-    gap: 20px;
-    padding: 15px;
-    border-radius: 10px;
-    background-color: ${(props: StyledProps) => (props.$isDark ? "#444" : "#f9f9f9")};
-    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-    transition: transform 0.2s ease, box-shadow 0.2s ease;
-
-    &:hover {
-      transform: translateY(-5px);
-      box-shadow: 0 6px 12px rgba(0, 0, 0, 0.15);
-    }
-  }
-
-  .thumbnail {
-		display: flex; 
-    align-items: center; 
-    justify-content: center; 
-    width: 120px;
-    height: 120px;
-    flex-shrink: 0;
-    border-radius: 8px;
-    background-size: cover;
-    background-position: center;
-    background-repeat: no-repeat;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-		margin-bottom: -2px;
-  }
-
-  .content {
-    flex-grow: 1;
-    display: flex;
-    flex-direction: column;
-		justify-content: center; 
-
-    .title {
-      font-size: 18px;
-      font-weight: bold;
-      margin-bottom: 5px;
-      color: ${(props: StyledProps) => (props.$isDark ? "#fff" : "#333")};
-    }
-
-    .summary {
-      font-size: 14px;
-      line-height: 1.4;
-      color: ${(props: StyledProps) => (props.$isDark ? "#ddd" : "#666")};
-    }
-
-    .meta {
-      font-size: 12px;
-      color: ${(props: StyledProps) => (props.$isDark ? "#aaa" : "#999")};
-      margin-top: 10px;
-    }
   }
 `;
 
@@ -208,25 +133,8 @@ const HomePage = () => {
 							{isListView ? <HiMiniSquares2X2 size={20} /> : <HiBars3 size={20} />}
 						</ToggleButton>
 					</ToggleWrapper>
-          {isListView ? (
-            <ListView $isDark={isDarkMode}>
-              {posts.map((post) => (
-                <div className="list-item" key={post.id}>
-                  <div
-                    className="thumbnail"
-                    style={{ backgroundImage: `url(${post.thumbnail})` }}
-                  ></div>
-                  <div className="content">
-                    <div className="title">{post.title}</div>
-                    <div className="summary">	{post.summary.length > 250 ? `${post.summary.slice(0,250)}...` : post.summary}</div>
-                    <div className="meta">Updated at: {post.updatedAt}</div>
-                  </div>
-                </div>
-              ))}
-            </ListView>
-          ) : (
-            <CardList posts={posts} />
-          )}
+          {isListView ? 
+          (<ListView posts={posts}/>) : (<CardList posts={posts} />)}
 				</Main>
 			</MainWrapper>
     </PageContainer>
