d2c4aad0490fbdf4902afd1f837288daa38fb1ff
jinsujj <wlstncjs1234@naver.com>
2025-03-22 10:56:11 +0900

    favIcon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,64 +4,68 @@ import StyledComponentsRegistry from "./registry";
 import "./globals.css";
 
 export const metadata = {
-  title: "부엉이 개발자 블로그",
-  description: "CTO가 되고픈 부엉이 블로그 입니다",
-  openGraph: {
-    url: "https://www.owl-dev.me",
-    title: "부엉이 개발자 블로그",
-    description: "CTO가 되고픈 부엉이 블로그 입니다",
-    images: [
-      {
-        url: "https://www.owl-dev.me/img/owl.svg",
-        width: 800,
-        height: 600,
-      },
-    ],
-  },
-  verification: {
-    google: "sxi1RDD-x-R6U-lHMRiV2kEtt-m7NVfNAaK-JoPyzTA",
-    naver: "857ab4a6de4aa0b0ddd2df29bcd1fb3129fe9198",
-  },
+	title: "부엉이 개발자 블로그",
+	description: "CTO가 되고픈 부엉이 블로그 입니다",
+	openGraph: {
+		url: "https://www.owl-dev.me",
+		title: "부엉이 개발자 블로그",
+		description: "CTO가 되고픈 부엉이 블로그 입니다",
+		images: [
+			{
+				url: "https://www.owl-dev.me/img/owl.svg",
+				width: 800,
+				height: 600,
+			},
+		],
+	},
+	verification: {
+		google: "sxi1RDD-x-R6U-lHMRiV2kEtt-m7NVfNAaK-JoPyzTA",
+		naver: "857ab4a6de4aa0b0ddd2df29bcd1fb3129fe9198",
+	},
 };
 
 
-function FontLinks() {
-  return (
-    <>
-      <link 
-        href="https://fonts.googleapis.com/css?family=Nanum+Gothic+Coding:400|Nanum+Gothic:400|Montserrat:wght@300;500;600&display=swap" 
-        rel="stylesheet" 
-      />
-    </>
-  );
+function Links() {
+	return (
+		<>
+			<link
+				href="https://fonts.googleapis.com/css?family=Nanum+Gothic+Coding:400|Nanum+Gothic:400|Montserrat:wght@300;500;600&display=swap"
+				rel="stylesheet"
+			/>
+			{/* favIcon */}
+			<link rel="shortcut icon" type="image/x-icon" href="/img/owl.svg" />
+			<link rel="icon" href="/img/owl.svg" />
+			<link rel="apple-touch-icon" href="/img/owl.svg" />
+		</>
+	);
 }
 
 
 function ExternalScripts() {
-  // const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID || '';
-  const NAVER_MAP_CLIENT_ID = process.env.NEXT_PUBLIC_NAVER_MAP_CLIENT_ID || '';
+	// const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID || '';
+	const NAVER_MAP_CLIENT_ID = process.env.NEXT_PUBLIC_NAVER_MAP_CLIENT_ID || '';
 
-  return (
-    <>
-      {/* External Scripts */}
-      <Script src="https://code.jquery.com/jquery-3.6.1.slim.min.js" strategy="lazyOnload" />
-      <Script 
-        src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" 
-        strategy="lazyOnload" />
-      <Script 
-        src="https://kit.fontawesome.com/3ec141240c.js" 
-        strategy="lazyOnload" />
-      <Script 
-        src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" 
-        strategy="lazyOnload" />
-      <Script 
-        src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js" 
-        strategy="lazyOnload" />
-      {/* Google Analytics */}
-      {/* <Script 
+	return (
+		<>
+			{/* External Scripts */}
+			<Script src="https://code.jquery.com/jquery-3.6.1.slim.min.js" strategy="lazyOnload" />
+			<Script
+				src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+				strategy="lazyOnload" />
+			<Script
+				src="https://kit.fontawesome.com/3ec141240c.js"
+				strategy="lazyOnload" />
+			<Script
+				src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"
+				strategy="lazyOnload" />
+			<Script
+				src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"
+				strategy="lazyOnload" />
+			{/* Google Analytics */}
+			{/* <Script 
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} 
         strategy="afterInteractive" /> */}
-      {/* <Script id="google-analytics" strategy="afterInteractive">
+			{/* <Script id="google-analytics" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
@@ -70,35 +74,35 @@ function ExternalScripts() {
             page_path: window.location.pathname,
           });
         `}</Script> */}
-      {/* Google AdSense */}
-      {/* <Script 
+			{/* Google AdSense */}
+			{/* <Script 
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2309486098831986" 
         strategy="afterInteractive" 
         crossOrigin="anonymous" /> */}
-      {/* Naver Map */}
-      <Script 
-        src={`https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${NAVER_MAP_CLIENT_ID}`} 
-        strategy="lazyOnload" />
-    </>
-  );
+			{/* Naver Map */}
+			<Script
+				src={`https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${NAVER_MAP_CLIENT_ID}`}
+				strategy="lazyOnload" />
+		</>
+	);
 }
 
 
-export default function RootLayout({children}: {children: React.ReactNode}) {
-  return (
-    <html lang="ko">
-      <head>
-        <FontLinks />
-      </head>
-      <body>
-        <StyledComponentsRegistry>
-          <ReduxProvider>
-            {children}
-          </ReduxProvider>
-        </StyledComponentsRegistry>
-        <div id="root-modal" />
-        <ExternalScripts />
-      </body>
-    </html>
-  );
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+	return (
+		<html lang="ko">
+			<head>
+				<Links />
+			</head>
+			<body>
+				<StyledComponentsRegistry>
+					<ReduxProvider>
+						{children}
+					</ReduxProvider>
+				</StyledComponentsRegistry>
+				<div id="root-modal" />
+				<ExternalScripts />
+			</body>
+		</html>
+	);
 }
